eb065ffd84f517f921787d64697da1f106499232
Kelvin Lin(林冠宇) <kelvin_lin@htc.com>
2015-05-28 12:48:20 +0000

    do clean to private function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,17 @@ class ProductsController < ApplicationController
 
    def create
      # strong parameter
-     clean_param = params.require(:product).permit(:name, :price)
-     @product = Product.new(clean_param)
+     #clean_param = params.require(:product).permit(:name,:price)
+     @product = Product.new(product_params)
      if @product.save
        redirect_to '/products'
      else
        redirect_to '/products'
      end
+   end
+
+   private
+   def product_params
+     return params.require(:product).permit(:name, :price)
    end
 end
