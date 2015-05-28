41be1b726f24c2213569e4f740c05e03fc65d205
Kelvin Lin(林冠宇) <kelvin_lin@htc.com>
2015-05-28 13:20:56 +0000

    use the fixtures 測試素材

---
+++ test/integration/products_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ProductsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "can post data to create page" do
+    
+    pro = products(:one)
+    puts pro.name
+    
+    c1 = Product.count
+    post "/products", product: {name: "rubyruby", price: 100}  
+    assert_redirected_to "/products"
+    assert_equal Product.count, c1 + 1
+  end
+  test "can not add product if name is empty " do
+    c1 = Product.count
+    post "/products", product: {name: "", price: 100}  
+    assert_redirected_to "/products"
+    assert_equal Product.count, c1 
+  end
+  #test "product must have name" do
+  #  assert_no_difference 'Product.count' do
+  #  post "/products", product: {name: "", description: "hello, ruby"}
+  #end
+end
